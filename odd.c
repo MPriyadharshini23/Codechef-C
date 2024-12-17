b5faef2d87bafb0d2e038e6244574b5a47aef57a
Priyadharshini M <mpriyadharshinicse23@gmail.com>
2024-12-17 14:32:25 +0530

    odd.c

---
+++ odd.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    // Update the code below this line
+    int i = 11;
+    while (i < 20) {
+        if(i%2!=0)
+        printf("%d\n",i);
+        i++;
+    }
+    return 0;
+}
